83f96b9f5fe9e897abd76db332585ef23e81b3c4
Michael Camilleri <mike@inqk.net>
2021-05-03 18:35:35 +0900

    Add Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    '**/*.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      borderRadius: {
+        '4xl': '2rem'
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
